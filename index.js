398ecf3545fe29b78483e187dfa69e7f878ea01a
Miriam Peskowitz <miriam5000@gmail.com>
2021-03-02 02:09:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function findMinAndRemove(array){
   let min = array[0]
   let minIndex = 0
-  array.forEach()
-  const min = Math.min(...[array])
+  for (let i = 0; i < array.length; i++) {
+  if (array[i] < min) {
+    min = array[i]
+    minIndex = i
+    }
+  }
   array.splice(minIndex, 1)
   return min
 }
 
 function selectionSort(array){
-
 }
